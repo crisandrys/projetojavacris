9e95aa5aa8277fdc14a66c26dd9acbad079d8fe2
crisandrys <160232496+crisandrys@users.noreply.github.com>
2024-07-26 12:02:34 -0300

    fv

---
+++ app.js
@@ -0,0 +1,11 @@
+alert('Boas vindas ao jogo do número secreto');
+let numeroSecreto = 17;
+console.log('numeroSecreto')
+let chute = prompt('Escolha um número entre 1 e 20');
+
+// se chute for igual ao número secreto
+if (chute == numeroSecreto) {
+alert('Isso ai! Você descobriu o número secreto (17)');
+} else {
+alert('Você errou :(')
+}
